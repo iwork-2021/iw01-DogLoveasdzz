6a1af82e3a33815a2ed833148f096395e15d6698
苏颖康 <suyingkang@suyingkangdeiMac-Pro.local>
2021-10-11 10:53:57 -0700

    first try

---
+++ Calculator/MyCalculator/MyCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by 苏颖康 on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
